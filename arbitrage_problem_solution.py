31922da397f0a30cb8b90178363d227aed07c4fa
Qinwu Xu <78186650+qinwuxutexas@users.noreply.github.com>
2022-12-20 22:09:33 -0500

    Update arbitrage_problem_solution.py

--- arbitrage_problem_solution.py
+++ arbitrage_problem_solution.py
@@ -1,16 +1,7 @@
 # a class for arbitrage - find max money value through currancy exchange chain (e.g., USD -> EU -> JPY -> USD, starts and ends at the same currency)
 # It uses the Dijkstra’s algorithm to find the money exchange path, wich is faster than the Bellmon-Ford algorithm
-
 # author: Q. Xu, Date: 12/03/2022, Version: 0.0
 # time complexity: O(ELogV), where E is number of edges, V is number of vertices/nodes
-
-''' other solutions:
-1) Program to Find Out Currency Arbitrage in Python, time cmplexity: o(V^3), V: number of vertices, a Brute solution
-    https://www.tutorialspoint.com/program-to-find-out-currency-arbitrage-in-python
-2) Bellman-Ford algorithm, time complexity: O(V*E), V: number of vertices, E: number of edges
-    https://etrain.github.io/finance/2013/06/08/currency-arbitrage-in-python
-
-'''
   
 import collections
 from typing import List
